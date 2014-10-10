760b4bab11196529e869ba8c290fd9ee3b16394e
klamlis <klamlis@gmail.com>
2014-10-10 10:09:05 -0500

    mod:20141010_1
    Se completo la tabla de los usuarios

---
+++ web/js/loginAjax.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+
+  $("#btn_entrar").click(function()
+  {
+
+    alert("click para Entrar");
+
+  });
+
+});
--- web/php/conexion.php
+++ web/php/conexion.php
@@ -1,10 +1,9 @@
 <?
-$dbhost="localhost";  // host del MySQL (generalmente localhost)
-$dbusuario="agustin"; // aqui debes ingresar el nombre de usuario
-                      // para acceder a la base
-$dbpassword="mipass"; // password de acceso para el usuario de la
-                      // linea anterior
-$db="ejemplo";        // Seleccionamos la base con la cual trabajar
-$conexion = mysql_connect($dbhost, $dbusuario, $dbpassword);
-mysql_select_db($db, $conexion);
+  $dbhost     ="localhost";
+  $dbusuario  ="root";
+  $dbpassword ="L1bertad0res";
+  $db         ="somae";
+  $conexion   = mysql_connect($dbhost, $dbusuario, $dbpassword);
+
+  mysql_select_db($db, $conexion);
 ?>
